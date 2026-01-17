a4997b10595366b62f0dfb14ac8c6aa3790663a2
Miquel Gómez <miquelgc2003@gmail.com>
2026-01-17 17:28:13 +0100

    reexecuted notebooks

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -3,10 +3,45 @@
 # Exit on error
 set -e
 
+NOTEBOOK_DIR="notebooks_pt_en"
+
 echo "Starting notebook execution..."
+echo "========================================"
 
+# Run non-extra notebooks first (L4.1, L4.2, L4.3, L4.4)
+echo ""
+echo "Running non-extra notebooks..."
+echo "========================================"
 
-echo "Running ES-EO-L3.5_NMT_LLAMA-3_Prompting.ipynb..."
-# jupyter nbconvert --to notebook --execute --inplace "notebooks/ES-EO-L3.5_NMT_LLAMA-3_Prompting.ipynb"
+echo "Running L4.1_ASR_Whisper_Baseline_Covost2.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/L4.1_ASR_Whisper_Baseline_Covost2.ipynb"
 
+echo "Running L4.2_ST_Whisper_Baseline_Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/L4.2_ST_Whisper_Baseline_Covost2-ST.ipynb"
+
+echo "Running L4.3_ST_Cascade_Whisper_NLLB_Baseline_Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/L4.3_ST_Cascade_Whisper_NLLB_Baseline_Covost2-ST.ipynb"
+
+echo "Running L4.4_ST_Cascade_Whisper_NLLB_Finetuning_Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/L4.4_ST_Cascade_Whisper_NLLB_Finetuning_Covost2-ST.ipynb"
+
+# Run extra notebooks
+echo ""
+echo "Running extra notebooks..."
+echo "========================================"
+
+echo "Running Extra1-L4.2_ST_Whisper_Baseline_Europarl-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/Extra1-L4.2_ST_Whisper_Baseline_Europarl-ST.ipynb"
+
+echo "Running Extra2.1-L4.1_ASR_Whisper_Baseline-Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/Extra2.1-L4.1_ASR_Whisper_Baseline-Covost2-ST.ipynb"
+
+echo "Running Extra2.2-L4.3_ST_Cascade_Whisper_MADLAD_Baseline_Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/Extra2.2-L4.3_ST_Cascade_Whisper_MADLAD_Baseline_Covost2-ST.ipynb"
+
+echo "Running Extra3-L4.4_ST_Cascade_Whisper_MADLAD_Finetuning_Covost2-ST.ipynb..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK_DIR/Extra3-L4.4_ST_Cascade_Whisper_MADLAD_Finetuning_Covost2-ST.ipynb"
+
+echo ""
+echo "========================================"
 echo "All notebooks executed successfully!"
